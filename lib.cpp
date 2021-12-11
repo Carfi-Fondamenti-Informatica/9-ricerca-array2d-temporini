5aa7c349ac2c0f27a1906cbb70a439aea868c5a9
temporini <91788264+temporini@users.noreply.github.com>
2021-12-11 20:48:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include <string.h>
+#include "lib.h"
 
+bool cerca(char lista[10][20], char nome[1][20], int &pos){
+
+    for(int i=0; i<10 ; i++){
+
+        if(strcmp (lista[i],nome[0]) == 0){
+            pos = i;
+            return true;
+        }
+    }
+
+    return false;
+}
